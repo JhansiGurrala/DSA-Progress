c684808e1930ff344f8ee376364284c9d8c2b728
JhansiGurrala <jhansigurrala521@gmail.com>
2024-11-16 15:33:30 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class index{
+    public static void main(String args[]){
+
+    Scanner jhansi = new Scanner(System.in);
+    int a=jhansi.nextInt();
+    int b=jhansi.nextInt();
+    int sum=a+b;
+    System.out.println(sum);
+    
+    }
+}   
